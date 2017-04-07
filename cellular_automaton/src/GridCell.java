8e2571120ffb4c74daf9bc6c21be3db2620dbb9a
Kristoffer Martinsen <kaminaribob@gmail.com>
2017-04-07 10:09:36 +0200

    Doing Things all the tim

--- cellular_automaton/src/GridCell.java
+++ cellular_automaton/src/GridCell.java
@@ -1,23 +1,30 @@
+package src;
+
 /**
  * Created by Kristoffer on 02.04.2017.
  */
 public class GridCell {
-    private String state;
+    private Boolean state;
+    private int particlesInCell;
 
     public GridCell() {
-
+        particlesInCell = 0;
     }
 
-    public void setState(String stateToSet) {
+    public void setState(Boolean stateToSet) {
         this.state = stateToSet;
     }
 
-    public String getState() {
+    public Boolean getState() {
         return state;
     }
 
-    public void location() {
+    public int getParticlesInCell() {
+        return particlesInCell;
+    }
 
+    public void setParticlesInCell(int particles) {
+        this.particlesInCell = particles;
     }
 
 }
--- cellular_automaton/src/Main.java
+++ cellular_automaton/src/Main.java
@@ -1,3 +1,4 @@
+package src;
 /**
  * Created by Kristoffer on 02.04.2017.
  */
@@ -6,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Simulator simulator = new Simulator();
         simulator.populate(10);
-        simulator.act(100);
+        //simulator.act(100);
     }
 }
